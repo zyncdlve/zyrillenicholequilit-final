75756c0b9e1124ace460133ed09525a00be55134
zyncdlve <153645477+zyncdlve@users.noreply.github.com>
2023-12-13 00:29:11 +0800

    Delete quilit_final directory

--- quilit_final/js/app.js
+++
@@ -1,5 +0,0 @@
-const navigation = document.querySelector("nav");
-const navigationHeight = navigation.offsetHeight;
-document.documentElement.style.setProperty(
-    "--scroll-padding", navigationHeight + "px"
-)
